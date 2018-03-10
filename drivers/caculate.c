6fe7dcfe7b5d9b5357eef4495e12d5859002c19a
greenstar <greenstar@email.com>
2018-03-10 06:59:14 +0000

    Sat 10 Mar 06:59:14 UTC 2018-----edition 1

---
+++ drivers/caculate.c
@@ -0,0 +1,44 @@
+#include  <unistd.h>
+#include  <sys/types.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+#include <pthread.h>
+#include "global_data.h"
+#include <semaphore.h>
+#include "include.h"
+#include <signal.h>
+
+///////////////////////////////////////////////
+/////////////////////////////////////////////////
+float kalman_filter(float last_result,float last_value,float cur_value,
+float time,float P_conv,float Q_offset,float R_offset,int flag)
+{
+float result=0;
+switch(flag)
+{
+	case 0:
+	{
+	float v_est=last_result+last_value*time;
+	float p_est=P_conv+Q_offset;
+	float kg=p_est/(p_est+Q_offset);
+	float v_mes=last_result+cur_value*time;
+	float v_best=last_result+kg(v_mes-v_est);
+	float p_best=(1-kg)p_est;
+	result=v_best;
+	}break;
+	case 1:
+	{
+	float pos_est=last_result+last_value*time;
+	float p_est=P_conv+Q_offset;
+	float kg=p_est/(p_est+Q_offset);
+	float pos_mes=last_result+cur_value*time;
+	float pos_best=last_result+kg(v_mes-v_est);
+	float p_best=(1-kg)p_est;
+	result=pos_best;
+	}break;
+	default:break;
+}
+return result;
+}
